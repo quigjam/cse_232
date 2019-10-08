a9994b11fac38539a0a2635e356618176b077610
James Quigley <quigle48@msu.edu>
2019-10-08 11:58:32 -0400

    added project1

---
+++ Project1/project.cpp
@@ -0,0 +1,13 @@
+
+
+int get_line_sum( string string_representation ) {
+    //it should take a string representing a line of CA and return an int that is the sum of all the CA digits.
+
+}
+
+char get_next_state(string neighborhood_cell, string mulitline_reference ){  
+    //returns a char and takes two strings. The first string represents a neighborhood of a cell (always length 3). The second parameter is a multiline string 
+    //A unspecified neighborhood  returns the default state of '0'.
+    //This multiline string is represented in C++ using raw string literals (see https://en.cppreference.com/w/cpp/language/string_literal).
+
+}
